064ad7007edaba277fb95c5132eb9f23dee1060d
Joshua Franz <joshua.franz@hs-furtwangen.de>
2022-08-09 01:11:41 +0200

    Experimental support for Buttons

--- src/main.cpp
+++ src/main.cpp
@@ -25,8 +25,8 @@ MAKE_HOOK_MATCH(PresentFlowCoordinatorHook, &HMUI::FlowCoordinator::PresentFlowC
     {
         // Save Controller for later use in button onClick
         auto *LevelSelectionFlowCoordinatorInstance = (GlobalNamespace::LevelSelectionFlowCoordinator *)flowCoordinator;
-        levelCollectionNavigationController = LevelSelectionFlowCoordinatorInstance->levelSelectionNavigationController->levelCollectionNavigationController;
-        filteringNavigationController = LevelSelectionFlowCoordinatorInstance->levelSelectionNavigationController->levelFilteringNavigationController;
+        levelCollectionNavigationController.emplace(LevelSelectionFlowCoordinatorInstance->levelSelectionNavigationController->levelCollectionNavigationController);
+        filteringNavigationController.emplace(LevelSelectionFlowCoordinatorInstance->levelSelectionNavigationController->levelFilteringNavigationController);
         
         if(button) {
             button->get_gameObject()->set_active(true);
@@ -57,42 +57,67 @@ MAKE_HOOK_MATCH(GamePlaySetUpHook, &GlobalNamespace::GameplaySetupViewController
     {
         // Create Randomselection Button
         button = QuestUI::BeatSaberUI::CreateUIButton(self->get_transform(), "", UnityEngine::Vector2(56.0f, 0), UnityEngine::Vector2(11.0f, 11.0f), []() {
-            // Only do Things, if we also have the necessary References
-            if(levelCollectionNavigationController && filteringNavigationController)
-            {
-                // Get Array of all beatmaps that the user currently sees
-                ArrayW<GlobalNamespace::IPreviewBeatmapLevel*> allmapsArray;
-                
-                auto* levelPack = levelCollectionNavigationController->levelPack;
-                if(levelPack) {
-                    allmapsArray = il2cpp_utils::cast<GlobalNamespace::IAnnotatedBeatmapLevelCollection>(levelPack)->get_beatmapLevelCollection()->get_beatmapLevels();
-                    getLogger().info("Acquired Maps using CollectionNavigationView");
-                }
-                else {
-                    allmapsArray = filteringNavigationController->levelSearchViewController->beatmapLevelPackCollection->get_beatmapLevelCollection()->get_beatmapLevels();
-                    getLogger().info("Acquired Maps using SearchView");
-                }
-
-                // Calculate Upper Bound for rand
-                int max = allmapsArray->Length();
-                getLogger().info("Acquired BeatMapCount");
-
-                if(max > 0)
-                {
-                    // Select a random level from 0 to max (exclusive)
-                    levelCollectionNavigationController->SelectLevel(allmapsArray->get(rand() % max));
-                    getLogger().info("Selected level");
-                }
-                else {
-                    getLogger().info("No level found to select");
-                }
-            } });
+            selectRandomSong();
+            });
 
         // Set Icon of the Button and scale it to fit the Button
         auto *image = QuestUI::BeatSaberUI::CreateImage(button->get_transform(), QuestUI::BeatSaberUI::Base64ToSprite(diceIcon));
         image->get_rectTransform()->set_localScale({0.65f, 0.65f, 1.0f});
 
         getLogger().info("Created Random Song Button");
+    }
+}
+
+MAKE_HOOK_MATCH(FixedUpdateHook, &GlobalNamespace::OculusVRHelper::FixedUpdate, void, GlobalNamespace::OculusVRHelper* self){
+    FixedUpdateHook(self);
+
+    // TODO Make configurable over config utils (all Buttons available)
+    bool AButtonPressed = GlobalNamespace::OVRInput::Get(GlobalNamespace::OVRInput::Button::One, GlobalNamespace::OVRInput::Controller::RTouch);
+    if(AButtonPressed){
+        if(!pressedEventAllreadyRun) {
+            selectRandomSong();
+            pressedEventAllreadyRun = true;
+        }
+    }
+    else {
+        pressedEventAllreadyRun = false;
+    }
+}
+
+// TODO Extract to own Class & File
+void selectRandomSong() {
+    // Only do Things, if we also have the necessary References
+    if (levelCollectionNavigationController && filteringNavigationController)
+    {
+        // Get Array of all beatmaps that the user currently sees
+        ArrayW<GlobalNamespace::IPreviewBeatmapLevel *> allmapsArray;
+
+        auto *levelPack = levelCollectionNavigationController->levelPack;
+        if (levelPack)
+        {
+            allmapsArray = il2cpp_utils::cast<GlobalNamespace::IAnnotatedBeatmapLevelCollection>(levelPack)->get_beatmapLevelCollection()->get_beatmapLevels();
+            getLogger().info("Acquired Maps using CollectionNavigationView");
+        }
+        else
+        {
+            allmapsArray = filteringNavigationController->levelSearchViewController->beatmapLevelPackCollection->get_beatmapLevelCollection()->get_beatmapLevels();
+            getLogger().info("Acquired Maps using SearchView");
+        }
+
+        // Calculate Upper Bound for rand
+        int max = allmapsArray->Length();
+        getLogger().info("Acquired BeatMapCount");
+
+        if (max > 0)
+        {
+            // Select a random level from 0 to max (exclusive)
+            levelCollectionNavigationController->SelectLevel(allmapsArray->get(rand() % max));
+            getLogger().info("Selected level");
+        }
+        else
+        {
+            getLogger().info("No level found to select");
+        }
     }
 }
 
@@ -115,5 +140,6 @@ extern "C" void load()
     getLogger().info("Installing hooks...");
     INSTALL_HOOK(getLogger(), PresentFlowCoordinatorHook);
     INSTALL_HOOK(getLogger(), GamePlaySetUpHook);
+    INSTALL_HOOK(getLogger(), FixedUpdateHook);
     getLogger().info("Installed all hooks!");
 }
